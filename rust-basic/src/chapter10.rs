4f835887f22ce2c687831b0933e67fb4be97fe30
chenmijiang <jack.chenyuana@gmail.com>
2025-12-19 06:40:10 +0800

    Refactor Point struct to support multiple types and implement mixup method in chapter 10

--- rust-basic/src/chapter10.rs
+++ rust-basic/src/chapter10.rs
@@ -16,18 +16,25 @@ pub fn main() {
     let result = largest(&char_list);
     println!("The largest char is {result}");
 
-    let integer_point = Point { x: 5, y: 10 };
-    println!("integer_point.x = {}", integer_point.x());
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
 
-struct Point<T> {
-    x: T,
-    y: T,
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
 }
 
-impl<P> Point<P> {
-    fn x(&self) -> &P {
-        &self.x
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
