8d8ed619996a8d9a868e52fe2e8fa2beca5dc465
chenmijiang <jack.chenyuana@gmail.com>
2025-12-21 11:25:39 +0800

    Refactor Guess struct validation to clarify panic messages and update test expectations

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -5,8 +5,10 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {value}.");
+        if value < 1 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
+        } else if value > 100 {
+            panic!("Guess value must be greater than or equal to 1, got {value}.");
         }
 
         Guess { value }
@@ -18,7 +20,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
