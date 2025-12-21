8aa5d15c6b288075d0e119ea5158264c4ab0560f
chenmijiang <jack.chenyuana@gmail.com>
2025-12-21 16:42:15 +0800

    Add tests for add_two function to verify correct behavior

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -19,9 +19,37 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn add_two(a: u64) -> u64 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn one_hundred() {
+        let result = add_two(100);
+        assert_eq!(result, 102);
+    }
 
     #[test]
     #[should_panic(expected = "less than or equal to 100")]
